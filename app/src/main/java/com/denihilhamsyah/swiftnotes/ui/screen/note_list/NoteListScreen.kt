724a5d26b7bd14f16ba11bb26da146f0be9715b5
Deni Hilhamsyah <denyhilhamsyh@gmail.com>
2023-01-29 03:16:48 +0700

    Adding Scaffold Adding Note Add FAB button

--- app/src/main/java/com/denihilhamsyah/swiftnotes/ui/screen/note_list/NoteListScreen.kt
+++ app/src/main/java/com/denihilhamsyah/swiftnotes/ui/screen/note_list/NoteListScreen.kt
@@ -1,15 +1,22 @@
 package com.denihilhamsyah.swiftnotes.ui.screen.note_list
 
+import android.widget.Toast
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.denihilhamsyah.swiftnotes.R
 import com.denihilhamsyah.swiftnotes.ui.components.NoteItem
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -17,15 +24,30 @@ import com.denihilhamsyah.swiftnotes.ui.components.NoteItem
 fun NoteListScreen() {
     val viewModel: NoteListViewModel = hiltViewModel()
     val notes = viewModel.notes.collectAsState(initial = emptyList())
+    val context = LocalContext.current
 
-    Column(modifier = Modifier.fillMaxWidth()) {
-        LazyVerticalStaggeredGrid(
-            columns = StaggeredGridCells.Fixed(2),
-            content = {
-                items(notes.value) {
-                    NoteItem(note = it)
-                }
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show()
+            }) {
+                Image(
+                    painter = painterResource(id = R.drawable.add_note),
+                    contentDescription = "add_note"
+                )
             }
-        )
-    }
+        },
+        content = { paddingValues ->
+            LazyVerticalStaggeredGrid(
+                modifier = Modifier.padding(paddingValues),
+                columns = StaggeredGridCells.Fixed(2),
+                content = {
+                    items(notes.value) {
+                        NoteItem(note = it)
+                    }
+                }
+            )
+        }
+    )
 }
